90b23140966a72a38739bd338b58b2901d0c47c1
Jakob Pau <jakob.commute@gmail.com>
2025-11-07 19:53:44 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const addButton = document.querySelector(".add-button");
+const addInput = document.querySelector(".add-input");
+const toDoList = document.querySelector(".to-do");
+const doneList = document.querySelector(".done-list");
+
+const aRight = document.querySelector(".button1");
+const aLeft = document.querySelector(".button2");
+
+addButton.addEventListener("click", () => {
+  if (addInput.value !== "") {
+    const newToDo = document.createElement("li");
+    const checkBox = document.createElement("input");
+    checkBox.type = "checkbox";
+    const text = document.createTextNode(addInput.value);
+
+    newToDo.appendChild(text);
+    newToDo.appendChild(checkBox);
+    toDoList.appendChild(newToDo);
+    addInput.value = "";
+
+    aRight.addEventListener("click", () => {
+      if (newToDo.parentElement === toDoList && checkBox.checked) {
+        doneList.appendChild(newToDo);
+        checkBox.checked = false;
+      }
+    });
+
+    aLeft.addEventListener("click", () => {
+      if (newToDo.parentElement === doneList && checkBox.checked) {
+        toDoList.appendChild(newToDo);
+        checkBox.checked = false;
+      }
+    });
+  }
+});
